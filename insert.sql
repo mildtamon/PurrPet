71096a31941cafc73afe24e7ed702988b964c6f0
pkrittin <61532699+pkrittin@users.noreply.github.com>
2024-03-19 17:43:40 +0700

    add insert.sql

---
+++ insert.sql
@@ -0,0 +1,4 @@
+INSERT INTO shops(shop_id,shop_name,shop_location) VALUES ('s01','Salaya pet shop',
+                '999 Phutthamonthon 4 Rd., Salaya, Phutthamonthon,Nakhon Pathom 73170');
+INSERT INTO shops(shop_id,shop_name,shop_location) VALUES ('s02','Samyan pet shop',
+                '944 Rama IV Rd, Wang Mai, Pathum Wan,Bangkok 10330');
