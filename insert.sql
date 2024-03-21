1812710e24762a956ab7d434f3039e8d74b792cd
pkrittin <61532699+pkrittin@users.noreply.github.com>
2024-03-22 01:12:49 +0700

    fix datatype in table and add some data

--- insert.sql
+++ insert.sql
@@ -1,4 +1,124 @@
+INSERT INTO products(prod_id, prod_name, prod_brand, prod_range, prod_desc, prod_type, animal_type, prod_barcode, price) VALUES
+                    ('p01','SmartHeart Roast Beef Flavor 500 g','SmartHeart','Adult, All Breeds','Enhanced Brain Function : DHA (from fish oil) and choline (from lecithin) for enhanced brain and nervous system function. ' ||
+                           'Healthy Heart : Omega3 fatty acids from fish oil for a healthy heart. ' ||
+                           'Healthy Skin and Coat : Balanced Omega 3 & 6 essential fatty acids for a healthy skin and shiny coat. ' ||
+                           'Immune System : Vitamin E and Selenium for strong immune system. ' ||
+                           'Healthy Digestion : Highly digestible ingredients for improved digestion and stool quality. ' ||
+                           'Strong Bones & Teeth : Calcium and Phosphorus for strong bones and teeth.',
+                    'food','dog','012345678912',45);
+INSERT INTO products(prod_id, prod_name, prod_brand, prod_range, prod_desc, prod_type, animal_type, prod_barcode, price) VALUES
+                    ('p02','SmartHeart Roast Beef Flavor 1.5 kg','SmartHeart','Adult, All Breeds','Enhanced Brain Function : DHA (from fish oil) and choline (from lecithin) for enhanced brain and nervous system function. ' ||
+                           'Healthy Heart : Omega3 fatty acids from fish oil for a healthy heart. ' ||
+                           'Healthy Skin and Coat : Balanced Omega 3 & 6 essential fatty acids for a healthy skin and shiny coat. ' ||
+                           'Immune System : Vitamin E and Selenium for strong immune system. ' ||
+                           'Healthy Digestion : Highly digestible ingredients for improved digestion and stool quality. ' ||
+                           'Strong Bones & Teeth : Calcium and Phosphorus for strong bones and teeth.',
+                    'food','dog','012345672313',139);
+INSERT INTO products(prod_id, prod_name, prod_brand, prod_range, prod_desc, prod_type, animal_type, prod_barcode, price) VALUES
+                    ('p03','SmartHeart Roast Beef Flavor 3.0 kg','SmartHeart','Adult, All Breeds','Enhanced Brain Function : DHA (from fish oil) and choline (from lecithin) for enhanced brain and nervous system function. ' ||
+                           'Healthy Heart : Omega3 fatty acids from fish oil for a healthy heart. ' ||
+                           'Healthy Skin and Coat : Balanced Omega 3 & 6 essential fatty acids for a healthy skin and shiny coat. ' ||
+                           'Immune System : Vitamin E and Selenium for strong immune system. ' ||
+                           'Healthy Digestion : Highly digestible ingredients for improved digestion and stool quality. ' ||
+                           'Strong Bones & Teeth : Calcium and Phosphorus for strong bones and teeth.',
+                    'food','dog','012345679414',240);
+INSERT INTO products(prod_id, prod_name, prod_brand, prod_range, prod_desc, prod_type, animal_type, prod_barcode, price) VALUES
+                    ('p04','SmartHeart Roast Beef Flavor 10.0 kg','SmartHeart','Adult, All Breeds','Enhanced Brain Function : DHA (from fish oil) and choline (from lecithin) for enhanced brain and nervous system function. ' ||
+                           'Healthy Heart : Omega3 fatty acids from fish oil for a healthy heart. ' ||
+                           'Healthy Skin and Coat : Balanced Omega 3 & 6 essential fatty acids for a healthy skin and shiny coat. ' ||
+                           'Immune System : Vitamin E and Selenium for strong immune system. ' ||
+                           'Healthy Digestion : Highly digestible ingredients for improved digestion and stool quality. ' ||
+                           'Strong Bones & Teeth : Calcium and Phosphorus for strong bones and teeth.',
+                    'food','dog','012345324415',755);
+INSERT INTO products(prod_id, prod_name, prod_brand, prod_range, prod_desc, prod_type, animal_type, prod_barcode, price) VALUES
+                    ('p05','SmartHeart Roast Beef Flavor 20.0 kg','SmartHeart','Adult, All Breeds','Enhanced Brain Function : DHA (from fish oil) and choline (from lecithin) for enhanced brain and nervous system function. ' ||
+                           'Healthy Heart : Omega3 fatty acids from fish oil for a healthy heart. ' ||
+                           'Healthy Skin and Coat : Balanced Omega 3 & 6 essential fatty acids for a healthy skin and shiny coat. ' ||
+                           'Immune System : Vitamin E and Selenium for strong immune system. ' ||
+                           'Healthy Digestion : Highly digestible ingredients for improved digestion and stool quality. ' ||
+                           'Strong Bones & Teeth : Calcium and Phosphorus for strong bones and teeth.',
+                    'food','dog','012345392416',1140);
+INSERT INTO products(prod_id, prod_name, prod_brand, prod_range, prod_desc, prod_type, animal_type, prod_barcode, price) VALUES
+                    ('p06','SmartHeart Chicken & Liver Flavor 450 g','SmartHeart','All Toy, Small Breeds','Enhanced Brain Function : DHA (from fish oil) and choline (from lecithin) for enhanced brain and nervous system function. ' ||
+                           'Healthy Heart : Omega3 fatty acids from fish oil for a healthy heart. ' ||
+                           'Healthy Skin and Coat : Balanced Omega 3 & 6 essential fatty acids for a healthy skin and shiny coat. ' ||
+                           'Immune System : Vitamin E and Selenium for strong immune system. ' ||
+                           'Healthy Digestion : Highly digestible ingredients for improved digestion and stool quality. ' ||
+                           'Strong Bones & Teeth : Calcium and Phosphorus for strong bones and teeth.',
+                    'food','dog','012373057819',55);
+INSERT INTO products(prod_id, prod_name, prod_brand, prod_range, prod_desc, prod_type, animal_type, prod_barcode, price) VALUES
+                    ('p07','SmartHeart Beef Steak Flavor 1.3 kg','SmartHeart','Adult, All Toy, Small Breeds','Enhanced Brain Function : DHA (from fish oil) and choline (from lecithin) for enhanced brain and nervous system function. ' ||
+                           'Healthy Heart : Omega3 fatty acids from fish oil for a healthy heart. ' ||
+                           'Healthy Skin and Coat : Balanced Omega 3 & 6 essential fatty acids for a healthy skin and shiny coat. ' ||
+                           'Immune System : Vitamin E and Selenium for strong immune system. ' ||
+                           'Healthy Digestion : Highly digestible ingredients for improved digestion and stool quality. ' ||
+                           'Strong Bones & Teeth : Calcium and Phosphorus for strong bones and teeth.',
+                    'food','dog','012373077820',150);
+INSERT INTO products(prod_id, prod_name, prod_brand, prod_range, prod_desc, prod_type, animal_type, prod_barcode, price) VALUES
+                    ('p08','Me-O Ocean Fish 450 g','Me-O','Adult, All Breeds','Complete and balanced nutrition for cats. ' ||
+                            'Taurine is an amino acid that is essential for the proper functioning of the eyes and to improve the cat''s eyesight. ' ||
+                            'Vitamin C boots cat''s immune system and helps reduce the detrimental effects of stress on your cat''s health. ' ||
+                            'Calcium, Phosphorus and Vitamin D help strengthen teeth and bones. ' ||
+                            'This Formula was developed to help prevent the risk of FLUTD (Feline Lower Urinary Tract Disease). ' ||
+                            'Omega 3 and Omega 6 from high quality oil in combination with Zinc will help nourish the cat''s hair and skin. ' ||
+                            'Lowsodium formula helps decrease the risks of high blood pressure, kidney and heart disease in cats',
+                    'food','cat','098764283945',68);
+INSERT INTO products(prod_id, prod_name, prod_brand, prod_range, prod_desc, prod_type, animal_type, prod_barcode, price) VALUES
+                    ('p09','Me-O Kitten Ocean Fish 400 g','Me-O','Kitten, All Breeds','Complete and balanced nutrition for cats. ' ||
+                            'Taurine is an amino acid that is essential for the proper functioning of the eyes and to improve the cat''s eyesight. ' ||
+                            'Vitamin C boots cat''s immune system and helps reduce the detrimental effects of stress on your cat''s health. ' ||
+                            'Calcium, Phosphorus and Vitamin D help strengthen teeth and bones. ' ||
+                            'This Formula was developed to help prevent the risk of FLUTD (Feline Lower Urinary Tract Disease). ' ||
+                            'Omega 3 and Omega 6 from high quality oil in combination with Zinc will help nourish the cat''s hair and skin. ' ||
+                            'Lowsodium formula helps decrease the risks of high blood pressure, kidney and heart disease in cats',
+                    'food','cat','098764244945',68);
+INSERT INTO products(prod_id, prod_name, prod_brand, prod_range, prod_desc, prod_type, animal_type, prod_barcode, price) VALUES
+                    ('p10','Me-O Tuna in Jelly for Senior Cat 7+ 80 g','Me-O','Senior, All Breeds','Made from real meat and real fish',
+                    'food','cat','098182048342',17);
+-- INSERT INTO products(prod_id, prod_name, prod_brand, prod_range, prod_desc, prod_type, animal_type, prod_barcode, price) VALUES
+--                     ('p11','','','','','','','',123);
+-- INSERT INTO products(prod_id, prod_name, prod_brand, prod_range, prod_desc, prod_type, animal_type, prod_barcode, price) VALUES
+--                     ('p12','','','','','','','',123);
+-- INSERT INTO products(prod_id, prod_name, prod_brand, prod_range, prod_desc, prod_type, animal_type, prod_barcode, price) VALUES
+--                     ('p13','','','','','','','',123);
+-- INSERT INTO products(prod_id, prod_name, prod_brand, prod_range, prod_desc, prod_type, animal_type, prod_barcode, price) VALUES
+--                     ('p14','','','','','','','',123);
+-- INSERT INTO products(prod_id, prod_name, prod_brand, prod_range, prod_desc, prod_type, animal_type, prod_barcode, price) VALUES
+--                     ('p15','','','','','','','',123);
+-- INSERT INTO products(prod_id, prod_name, prod_brand, prod_range, prod_desc, prod_type, animal_type, prod_barcode, price) VALUES
+--                     ('p16','','','','','','','',123);
+-- INSERT INTO products(prod_id, prod_name, prod_brand, prod_range, prod_desc, prod_type, animal_type, prod_barcode, price) VALUES
+--                     ('p17','','','','','','','',123);
+-- INSERT INTO products(prod_id, prod_name, prod_brand, prod_range, prod_desc, prod_type, animal_type, prod_barcode, price) VALUES
+--                     ('p18','','','','','','','',123);
+-- INSERT INTO products(prod_id, prod_name, prod_brand, prod_range, prod_desc, prod_type, animal_type, prod_barcode, price) VALUES
+--                     ('p19','','','','','','','',123);
+-- INSERT INTO products(prod_id, prod_name, prod_brand, prod_range, prod_desc, prod_type, animal_type, prod_barcode, price) VALUES
+--                     ('p20','','','','','','','',123);
+--
+-- INSERT INTO orders(trans_id, prod_id, trans_date, lot_number, exp_date, amount, cost) VALUES
+--                     ('','',2023-11-11,'',2025-08-11,10,232);
+
+
+
 INSERT INTO shops(shop_id,shop_name,shop_location) VALUES ('s01','Salaya pet shop',
-                '999 Phutthamonthon 4 Rd., Salaya, Phutthamonthon,Nakhon Pathom 73170');
+                '999 Phutthamonthon 4 Rd., Salaya, Phutthamonthon, Nakhon Pathom 73170');
 INSERT INTO shops(shop_id,shop_name,shop_location) VALUES ('s02','Samyan pet shop',
                 '944 Rama IV Rd, Wang Mai, Pathum Wan,Bangkok 10330');
+INSERT INTO shops(shop_id,shop_name,shop_location) VALUES ('w01','Ladkrabang warehouse',
+                '11/6 Patthana Chonabot 3 Rd., Khlong Song Ton Nun, Lat Krabang, Bangkok, 10520');
+
+INSERT INTO staffs(staff_id, staff_name, staff_citizen_id, staff_email, staff_contact, staff_bank_book, staff_address, staff_type) VALUES
+                    ('s00','dummy','0123456789123','admin@gmail.com','0987654321','1234567890','123 Phutthamonthon 3 Rd., Salaya, Phutthamonthon, Nakhon Pathom 73170','admin');
+INSERT INTO staffs(staff_id, staff_name, staff_citizen_id, staff_email, staff_contact, staff_bank_book, staff_address, staff_type) VALUES
+                    ('s01','mild','2957393640128','mild@gmail.com','0627491623','1730593261','13/34 Building 12 V-condo, Soi Tang-sin, Salaya, Phutthamonthon, Nakhon Pathom 73170','cashier');
+INSERT INTO staffs(staff_id, staff_name, staff_citizen_id, staff_email, staff_contact, staff_bank_book, staff_address, staff_type) VALUES
+                    ('s02','max','205480481354','max@gmail.com','0620371294','1903739561','56/33 Salaya-One, Soi Tang-sin, Salaya, Phutthamonthon, Nakhon Pathom 73170','cashier');
+
+INSERT INTO customers(customer_id, customer_name, customer_email, customer_contact) VALUES
+                        ('c00','guest','guest@gmail.com','0000000000');
+INSERT INTO customers(customer_id, customer_name, customer_email, customer_contact) VALUES
+                        ('c01','alex','alex@gmail.com','0284750183');
+INSERT INTO customers(customer_id, customer_name, customer_email, customer_contact) VALUES
+                        ('c02','bob','bob@gmail.com','0926783628');
+
