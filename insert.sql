a85d40568fecd1ec24455974db5c843df3b477be
pkrittin <61532699+pkrittin@users.noreply.github.com>
2024-03-28 22:33:05 +0700

    update something

--- insert.sql
+++ insert.sql
@@ -134,6 +134,7 @@ INSERT INTO products(prod_name, prod_img_URL, prod_brand, prod_range, prod_desc,
 
 -- insert orders (from outside to warehouse)
 -- เปลี่ยนเป็นเรียกจาก order.orderProduct()
+-- call
 -- INSERT INTO orders(trans_id,receipt_img_URL, prod_id, trans_date, exp_date, amount, cost_per_unit, total_cost) VALUES
 --                     ('t00001','bit.ly/ashkasdlj',1,'2023-11-11 14:30:15','2025-08-11',100,35, 3500);
 -- INSERT INTO orders(trans_id,receipt_img_URL, prod_id, trans_date, exp_date, amount, cost_per_unit,total_cost) VALUES
@@ -142,16 +143,16 @@ INSERT INTO products(prod_name, prod_img_URL, prod_brand, prod_range, prod_desc,
 
 -- INSERT STOCKS (will be change after orders is finished)
 -- insert stocks (from outside to warehouse)
-call addtowarehouse(1,'storage #1 shelf #1','t00001',100);
-call addtowarehouse(2,'storage #1 shelf #2','t00002',50);
-call addtowarehouse(6,'storage #1 shelf #3','t00003',25);
-call addtowarehouse(10,'storage #1 shelf #3','t00004',10);
+-- call addtowarehouse(1,'storage #1 shelf #1','t00001',100);
+-- call addtowarehouse(2,'storage #1 shelf #2','t00002',50);
+-- call addtowarehouse(6,'storage #1 shelf #3','t00003',25);
+-- call addtowarehouse(10,'storage #1 shelf #3','t00004',10);
 
 -- insert stocks (from warehouse to branches)
-call distribute(2, 1, 60, 't00001');
-call distribute(2, 2, 20, 't00002');
-call distribute(3, 2, 10, 't00002');
-call distribute(3, 10, 5, 't00004');
+-- call distribute(2, 1, 60, 't00001');
+-- call distribute(2, 2, 20, 't00002');
+-- call distribute(3, 2, 10, 't00002');
+-- call distribute(3, 10, 5, 't00004');
 
 -- TODO: (mild) add to distributions
 
